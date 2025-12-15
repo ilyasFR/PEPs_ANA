5d1c537d137480cfa3ad254968427c1f2986bd60
clement-vazeille <clement.vazeille@gmail.com>
2025-12-15 16:15:30 +0100

    Added tests

---
+++ back/PEPs_back/src/test/java/peps/peps_back/controllers/DailyStatsControllerTest.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package peps.peps_back.controllers;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.http.ResponseEntity;
+
+/**
+ *
+ * @author Clément
+ */
+public class DailyStatsControllerTest {
+    
+    public DailyStatsControllerTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getDailyStats method, of class DailyStatsController.
+     */
+    @Test
+    public void testGetDailyStats() {
+        System.out.println("getDailyStats");
+        DailyStatsController instance = new DailyStatsController();
+        ResponseEntity<List<DailyDataDTO>> expResult = null;
+        ResponseEntity<List<DailyDataDTO>> result = instance.getDailyStats();
+        assertEquals(expResult, result);
+    }
+    
+}
